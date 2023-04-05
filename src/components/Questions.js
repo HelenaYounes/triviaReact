4b60a75d3c5cb85d92e0e62b69716f54af5df384
Héléna Younes <younesq.helena@gmail.com>
2023-04-04 23:56:10 -0700

    added question state in question component, on click of answer, turns red or green if valid or not

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,28 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { CCallout, CListGroup, CListGroupItem } from "@coreui/react";
+import { CCallout, CListGroup, CListGroupItem, CButton } from "@coreui/react";
 import Score from "./Score";
 
 const Questions = ({ questions, totalScore, dispatch }) => {
   const navigate = useNavigate();
-  const [currentScore, setCurrentScore] = useState(0);
+  const commencingState = {
+    idPicked: null,
+    isSelected: false,
+    colorAns: "light",
+  };
+  const [stateQ, setStateQ] = useState(commencingState);
   const [currentQ, setCurrentQ] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+
   const question = questions[currentQ].question;
   const answer = questions[currentQ].correctAnswer;
   const incorrects = questions[currentQ].incorrectAnswers;
-  const choices = [...incorrects, answer];
+  const choices = [...incorrects, answer].sort();
 
-  const checkAnswer = (e) => {
-    if (e.target.innerHTML === answer) {
+  const choiceHandler = (e) => {
+    let pick = "light";
+    if (!stateQ.isSelected) {
+      e.target.innerHTML === answer ? (pick = "success") : (pick = "danger");
+    }
+
+    setStateQ({
+      ...stateQ,
+      idPicked: e.target.attributes.id.value,
+      isSelected: true,
+      colorAns: pick,
+    });
+  };
+
+  const nextQuestion = () => {
+    setStateQ(commencingState);
+    if (currentQ < questions.length - 1) {
+      setCurrentQ(currentQ + 1);
+    } else {
       dispatch({
         type: "increaseTotalScore",
-        payload: { totalScore: totalScore + 1 },
+        payload: { totalScore: totalScore + currentScore },
       });
-      setCurrentScore(currentScore + 1);
-    };
-    currentQ < questions.length - 1
-      ? setCurrentQ(currentQ + 1)
-      : navigate("/home");
+      setCurrentScore(0);
+      navigate("/home");
+    }
   };
 
   return (
@@ -30,18 +52,39 @@ const Questions = ({ questions, totalScore, dispatch }) => {
       <Score score={currentScore} />
       <CCallout color="primary">{question}</CCallout>
       <CListGroup>
-        {choices.map((choice) => {
-          return (
+        {choices.map((choice, index) => {
+          return choice === answer ? (
             <CListGroupItem
-              key={Math.random()}
-              component="button"
-              onClick={(e) => checkAnswer(e)}
+              key={index}
+              id={index}
+              color={
+                stateQ.idPicked && index == stateQ.idPicked
+                  ? stateQ.colorAns
+                  : "light"
+              }
+              onClick={choiceHandler}
+            >
+              {choice}
+            </CListGroupItem>
+          ) : (
+            <CListGroupItem
+              key={index}
+              id={index}
+              color={
+                stateQ.idPicked && index == stateQ.idPicked
+                  ? stateQ.colorAns
+                  : "light"
+              }
+              onClick={choiceHandler}
             >
               {choice}
             </CListGroupItem>
           );
         })}
       </CListGroup>
+      <CButton color="dark" variant="outline" onClick={nextQuestion}>
+        Next question
+      </CButton>
     </div>
   );
 };
